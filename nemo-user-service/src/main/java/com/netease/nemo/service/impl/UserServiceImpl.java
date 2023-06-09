4ce1afb9f6c7eead7cf7effbb84d34dc140897f6
caohao02 <caohao02@corp.netease.com>
2023-06-09 16:09:08 +0800

    优化用户查询

--- nemo-user-service/src/main/java/com/netease/nemo/service/impl/UserServiceImpl.java
+++ nemo-user-service/src/main/java/com/netease/nemo/service/impl/UserServiceImpl.java
@@ -8,25 +8,31 @@ import com.netease.nemo.mapper.UserDeviceMapper;
 import com.netease.nemo.mapper.UserMapper;
 import com.netease.nemo.model.po.User;
 import com.netease.nemo.model.po.UserDevice;
+import com.netease.nemo.openApi.NeRoomService;
 import com.netease.nemo.openApi.NimService;
 import com.netease.nemo.openApi.enums.FriendAddTypeEnum;
 import com.netease.nemo.openApi.enums.ImMsgTypeEnum;
 import com.netease.nemo.openApi.enums.ImOpeEnum;
+import com.netease.nemo.openApi.paramters.neroom.CreateNeRoomUserParam;
 import com.netease.nemo.parameter.CreateUserParam;
 import com.netease.nemo.parameter.LoginParam;
 import com.netease.nemo.parameter.UpdateUserParam;
 import com.netease.nemo.service.UserService;
 import com.netease.nemo.util.ObjectMapperUtil;
 import com.netease.nemo.util.UUIDUtil;
+import com.netease.nemo.wrapper.UserMapperWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -36,7 +42,14 @@ public class UserServiceImpl implements UserService {
     private NimService nimService;
 
     @Resource
+    private NeRoomService neRoomService;
+
+    @Resource
     private UserMapper userMapper;
+
+    @Resource
+    private UserMapperWrapper userMapperWrapper;
+
     @Resource
     private UserDeviceMapper userDeviceMapper;
 
@@ -47,23 +60,24 @@ public class UserServiceImpl implements UserService {
     private String yunxinAssistAccid;
 
     @Override
+    @Transactional
     public UserDto login(LoginParam loginParam) {
         String userUuid = loginParam.getUserUuid();
         String deviceId = loginParam.getDeviceId();
 
-        User user = userMapper.selectByUserUuid(userUuid);
+        User user = userMapperWrapper.selectByUserUuid(userUuid);
         if (user == null) {
             user = ObjectMapperUtil.map(loginParam, User.class);
             user.setUserToken(UUIDUtil.getRandomString(20));
             user.setState(UserStateEnum.NORMAL.getState());
             user.setImToken(UUIDUtil.getRandomString(20));
-            userMapper.insertSelective(user);
+            userMapperWrapper.insertSelective(user);
         }
 
-        if(BooleanUtils.isTrue(loginParam.getIsCreateImAccId())) {
+        if (BooleanUtils.isTrue(loginParam.getIsCreateImAccId())) {
             nimService.createUser(userUuid, user.getUserName(), user.getIcon(), user.getImToken());
         }
-        if(BooleanUtils.isTrue(loginParam.getIsAddFriend())) {
+        if (BooleanUtils.isTrue(loginParam.getIsAddFriend())) {
             nimService.addFriend(userUuid, yunxinAssistAccid, FriendAddTypeEnum.ADD_FRIEND.getCode(), "添加云信小秘书");
             nimService.sendImMsg(yunxinAssistAccid, userUuid, ImOpeEnum.SINGLE_CHAT_MESSAGE.getOpe(), ImMsgTypeEnum.TEXT_MESSAGE.getType(), "欢迎来到\"云信派对\"，一场奇妙的旅行就此开始。");
         }
@@ -74,7 +88,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto login(String userUuid, String deviceId) {
-        User user = userMapper.selectByUserUuid(userUuid);
+        User user = userMapperWrapper.selectByUserUuid(userUuid);
         UserDevice userDevice = getUserDevice(userUuid, deviceId);
         return UserDto.build(user, userDevice);
     }
@@ -82,16 +96,25 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUser(String userUuid) {
-        User user = userMapper.selectByUserUuid(userUuid);
+        User user = userMapperWrapper.selectByUserUuid(userUuid);
         if (user == null) {
-            throw  new BsException(ErrorCode.USER_NOT_EXIST);
+            throw new BsException(ErrorCode.USER_NOT_EXIST);
         }
         return UserDto.build(user);
     }
 
     @Override
+    public List<UserDto> getUsers(List<String> userUuids) {
+        if (CollectionUtils.isEmpty(userUuids)) {
+            return Collections.emptyList();
+        }
+        List<User> users = userMapper.selectByUserUuids(userUuids);
+        return users.stream().map(UserDto::build).collect(Collectors.toList());
+    }
+
+    @Override
     public UserDto getUserByUidAndDeviceId(String userUuid, String deviceId) {
-        User user = userMapper.selectByUserUuid(userUuid);
+        User user = userMapperWrapper.selectByUserUuid(userUuid);
         UserDevice userDevice = getUserDevice(userUuid, deviceId);
         return UserDto.build(user, userDevice);
     }
@@ -105,7 +128,7 @@ public class UserServiceImpl implements UserService {
         if (userDevice == null) {
             return null;
         }
-        User user = userMapper.selectByUserUuid(userDevice.getUserUuid());
+        User user = userMapperWrapper.selectByUserUuid(userDevice.getUserUuid());
         if (user == null) {
             return null;
         }
@@ -114,30 +137,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto getUserByMobile(String mobile) {
-        User user = userMapper.getUserByMobile(mobile);
+        User user = userMapperWrapper.getUserByMobile(mobile);
         if (user == null) {
-            throw  new BsException(ErrorCode.USER_NOT_EXIST);
+            throw new BsException(ErrorCode.USER_NOT_EXIST);
         }
         return UserDto.build(user);
     }
 
     @Override
+    @Transactional
     public UserDto createUser(CreateUserParam createUserParam) {
         String userUuid = createUserParam.getUserUuid();
-        User user = userMapper.selectByUserUuid(userUuid);
+        User user = userMapperWrapper.selectByUserUuid(userUuid);
         if (user != null) {
             log.info("账号已存在");
+            // 不做更新直接返回老账号
             UserDevice userDevice = getUserDevice(userUuid, createUserParam.getDeviceId());
             return UserDto.build(user, userDevice);
         }
 
         user = ObjectMapperUtil.map(createUserParam, User.class);
         user.setState(UserStateEnum.NORMAL.getState());
-        userMapper.insertSelective(user);
-        if(BooleanUtils.isTrue(createUserParam.getIsCreateImAccId())) {
+        userMapperWrapper.insertSelective(user);
+        if (BooleanUtils.isTrue(createUserParam.getIsCreateImAccId())) {
             nimService.createUser(userUuid, user.getUserName(), user.getIcon(), user.getImToken());
         }
-        if(BooleanUtils.isTrue(createUserParam.getIsAddFriend())) {
+        if (BooleanUtils.isTrue(createUserParam.getIsAddFriend())) {
             nimService.addFriend(userUuid, yunxinAssistAccid, FriendAddTypeEnum.ADD_FRIEND.getCode(), "添加云信小秘书");
             nimService.sendImMsg(yunxinAssistAccid, userUuid, ImOpeEnum.SINGLE_CHAT_MESSAGE.getOpe(), ImMsgTypeEnum.TEXT_MESSAGE.getType(), "欢迎来到\"云信派对\"，一场奇妙的旅行就此开始。");
         }
@@ -146,10 +171,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void updateUser(UpdateUserParam updateUserParam) {
-        User user = userMapper.selectByUserUuid(updateUserParam.getUserUuid());
+        User user = userMapperWrapper.selectByUserUuid(updateUserParam.getUserUuid());
         if (user == null) {
-            throw  new BsException(ErrorCode.USER_NOT_EXIST);
+            throw new BsException(ErrorCode.USER_NOT_EXIST);
         }
 
         if (StringUtils.isNotEmpty(updateUserParam.getUserName())) {
@@ -164,53 +190,45 @@ public class UserServiceImpl implements UserService {
         if (null != updateUserParam.getSex()) {
             user.setSex(updateUserParam.getSex());
         }
-        userMapper.updateByPrimaryKey(user);
+        userMapperWrapper.updateByPrimaryKeySelective(user);
     }
 
     @Override
-    public List<UserDto> initOneToOneTestUser() {
-        List<UserDto> testUsers = new ArrayList<>();
-        // 初始化 nimSystemBot
-        if (userMapper.selectByUserUuid(nimSystemBot) == null) {
-            initDefaultUser("10333333331", nimSystemBot, "云信派对后台账号", "https://yx-web-nosdn.netease.im/common/d806ef46c9e6f53c73545786e76a5648/POPO20230419-204253.png");
+    @Transactional
+    public UserDto createNeRoomUser(CreateUserParam createUserParam) {
+        String userUuid = createUserParam.getUserUuid();
+        String imToken = createUserParam.getImToken();
+        if(StringUtils.isEmpty(imToken)) {
+            imToken = UUIDUtil.getUUID();
         }
-        // 初始化 yunxinAssistAccid
-        if(userMapper.selectByUserUuid(yunxinAssistAccid) == null) {
-            initDefaultUser("10333333332", yunxinAssistAccid, "云信派对小秘书","https://yx-web-nosdn.netease.im/common/d806ef46c9e6f53c73545786e76a5648/POPO20230419-204253.png");
-        }
-
-        // 初始化 yunXinPartyTest1
-        User yunXinPartyTest1 = userMapper.selectByUserUuid("yunxin_party_test1");
-        if (yunXinPartyTest1 == null) {
-            testUsers.add(initDefaultUser("11333333333", "yunxin_party_test1", "yunxin_party_test1", ""));
-            nimService.addFriend("yunXinPartyTest1", yunxinAssistAccid, FriendAddTypeEnum.ADD_FRIEND.getCode(), "添加云信小秘书");
-        } else {
-            testUsers.add(UserDto.build(yunXinPartyTest1));
+        String userToken = createUserParam.getUserToken();
+        if(StringUtils.isEmpty(userToken)) {
+            userToken = UUIDUtil.getUUID();
         }
 
-        // 初始化 yunXinPartyTest2
-        User yunXinPartyTest2 = userMapper.selectByUserUuid("yunxin_party_test2");
-        if (yunXinPartyTest2 == null) {
-            testUsers.add(initDefaultUser("11333333334", "yunxin_party_test2", "yunxin_party_test2", ""));
-            nimService.addFriend("yunxin_party_test2", yunxinAssistAccid, FriendAddTypeEnum.ADD_FRIEND.getCode(), "添加云信小秘书");
-        } else {
-            testUsers.add(UserDto.build(yunXinPartyTest2));
+        User user = userMapperWrapper.selectByUserUuid(userUuid);
+        if (user != null) {
+            log.info("账号已存在,使用老账号同步");
+            // 不做更新直接返回老账号
+            return UserDto.build(user);
         }
 
-        return testUsers;
-    }
-
-    private UserDto initDefaultUser(String mobile, String userUuid, String useName, String icon) {
-        User user = new User();
-        user.setUserUuid(userUuid);
-        user.setMobile(mobile);
-        user.setUserName(useName);
-        user.setIcon(icon);
-        user.setUserToken(UUIDUtil.getRandomString(20));
+        user = ObjectMapperUtil.map(createUserParam, User.class);
         user.setState(UserStateEnum.NORMAL.getState());
-        user.setImToken(UUIDUtil.getRandomString(20));
-        userMapper.insertSelective(user);
-        nimService.createUser(userUuid, useName, icon, user.getImToken());
+        user.setImToken(imToken);
+        user.setUserToken(userToken);
+
+        // 同步NeRoom账号
+        CreateNeRoomUserParam neRoomUser = new CreateNeRoomUserParam();
+        neRoomUser.setUserName(user.getUserName());
+        neRoomUser.setIcon(user.getIcon());
+        neRoomUser.setImToken(imToken);
+        neRoomUser.setUserToken(userToken);
+        neRoomUser.setUpdateOnConflict(true);
+        neRoomService.createNeRoomUser(userUuid, neRoomUser);
+
+        userMapperWrapper.insertSelective(user);
+
         return UserDto.build(user);
     }
 
